4465d26aab4392b56a7aa65c9cdb52b4ee752979
kj-rea <ksawery.jasienski@reach-initiative.org>
2022-08-05 18:08:34 +0200

    add check for duplicates in choice labels

--- check_kobo.R
+++ check_kobo.R
@@ -21,7 +21,7 @@ check_answer_in_list <- function(questions, choices, constraint){
   }
   
 }
-
+# Q: perhaps change these two functions to work on tool.survey and not questions, choices?
 check_constraints <- function(questions, choices){
   
   questions <- mutate_at(questions, c("name", "type"), ~str_trim(.))
@@ -37,4 +37,20 @@ check_constraints <- function(questions, choices){
          return(unlist(ex_default(all_contraints[.y], pattern = "selected\\s*\\([^\\)]*\\)"))[which(!.x)])
        }) %>% 
     unlist() %>% unique()
+}
+
+check_choice_labels_duplicates <- function(){
+  #' @returns vector containing columns that have duplicated labels in choices
+  dups <- c()
+  for (q in tool.survey$name){
+    # if (!(select_one.cols[i] %in% cols_exclude)){
+    choices1 <- filter(tool.choices, list_name==get.choice.list.from.name(q))[[label_colname]]
+    choices <- unique(choices1)
+    if(length(choices1)!=length(choices)){
+      dups <- append(dups, q)
+      warning(paste("Found duplicated choice labels in tool.choices for",q,
+                                                        "\t|\tCulprit choice list:",get.choice.list.from.name(q)))
+    }
+  }
+  return(dups)
 }
